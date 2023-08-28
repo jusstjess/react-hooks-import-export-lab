a750e7d90f74a9221a49d709acdaa6b18f18a0c8
Jess Leyva <jessica.leyvat@yahoo.com>
2023-08-27 20:35:54 -0700

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
+import NavBar from "./NavBar"
+import {username,city,image} from "../data/user";
 
 function App() {
+
   return (
     <div>
       <NavBar />
